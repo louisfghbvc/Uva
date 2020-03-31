a45e48731b8c1445918bff298d2ceb11552caaed
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-03-31 23:25:56 +0800

    Simple. strchr()

---
+++ String/Uva 10222 Decode the Mad man.cpp
@@ -0,0 +1,19 @@
+//useful strchr. cool. and in c/c++ pointer is array, same
+#include <bits/stdc++.h>
+using namespace std;
+const char *lst = "`1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+int main(){
+    string s;
+    while(getline(cin, s)){
+    	for(int i = 0; i < s.size(); ++i){
+    		s[i] = tolower(s[i]);
+    		char *tmp = strchr(lst, s[i]);
+    		if(tmp)
+    			printf("%c", lst[tmp - lst - 2]);
+    		else
+    			printf("%c", s[i]);
+    	}
+    	puts("");
+    }
+    return 0;
+}
