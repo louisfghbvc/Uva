2d3eac401c975c7a0de3f7a09761c9ca543961d5
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-04-03 00:29:01 +0800

    Simple

--- Array/Uva 11063 B2-Sequence.cpp
+++ Array/Uva 11063 B2-Sequence.cpp
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+int arr[105];
 int main(){
     int n, cas = 1, t;
     while(cin >> n){
     	bool ok = 1;
     	for(int i = 0; i < n; ++i){
-    		cin >> t;
-    		if(__builtin_popcount(t) > 1) ok = 0;
+    		cin >> arr[i];
+    	}
+    	bool vis[20005] = {};
+    	for(int i = 0; i < n && ok; ++i){
+    	   for(int j = i; j < n && ok; ++j){
+    	       if(vis[arr[i]+arr[j]]) ok = 0;
+    	       vis[arr[i]+arr[j]] = 1;
+    	   }    
     	}
     	printf("Case #%d: ", cas++);
     	puts(ok ? "It is a B2-Sequence.\n": "It is not a B2-Sequence.\n");
