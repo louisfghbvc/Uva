b7bc866f5e8b0493e8844ee63fb0a6d35aaa5aef
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-04-02 23:56:38 +0800

    Simple, O(sqrt(N)), Faster

--- Math/Uva 11461 Square Numbers.cpp
+++ Math/Uva 11461 Square Numbers.cpp
@@ -4,6 +4,22 @@ int main(){
     int a, b;
     while(scanf("%d %d", &a, &b) != EOF && a+b){
     	int ans = 0;
+    	for(int i = sqrt(a); i <= sqrt(b); ++i){
+    		if(i*i >= a && i*i <= b) ans++;
+    	}
+    	printf("%d\n", ans);
+    }
+    return 0;
+}
+
+/**
+//O(N)
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b;
+    while(scanf("%d %d", &a, &b) != EOF && a+b){
+    	int ans = 0;
     	for(int i = a; i <= b; ++i){
     		int t = sqrt(i) + 1e-6;
     		if(t*t == i) ans++;
@@ -12,3 +28,4 @@ int main(){
     }
     return 0;
 }
+**/
