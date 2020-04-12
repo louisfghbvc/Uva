5d776940ba3a45d9594c531de9aaaa5808b755e7
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-04-12 19:20:01 +0800

    BigInt 壓縮

--- BigInt/Uva 495 [Like And Useful]Fibonacci Freeze.cpp
+++ BigInt/Uva 495 [Like And Useful]Fibonacci Freeze.cpp
@@ -1,4 +1,4 @@
-import java.util.*;
+/*import java.util.*;
 import java.math.BigInteger;
 public class Main{
     public static void main(String[] args) {
@@ -23,3 +23,33 @@ public class Main{
         
     }
 };
+*/
+//C++ BigInt
+#include <bits/stdc++.h>
+#define N 305
+using namespace std;
+
+int dp[5001][N];
+
+int main(){
+	dp[1][0] = 1;
+	for(int i = 2; i <= 5000; ++i){
+		for(int j = 0; j < N; ++j)
+			dp[i][j] = dp[i-1][j]+dp[i-2][j];
+		for(int j = 0; j < N; ++j){
+			dp[i][j+1] += dp[i][j]/10000;
+			dp[i][j] %= 10000;
+		}
+	}
+    int n;
+    while(scanf("%d", &n) != EOF){
+    	int i;
+    	for(i = N-1; i >= 0 && !dp[n][i]; --i);
+    	printf("The Fibonacci number for %d is ", n);
+    	printf("%d", dp[n][i--]);
+    	for(; i >= 0; --i)
+    		printf("%04d", dp[n][i]);
+    	puts("");
+    }
+    return 0;
+}
