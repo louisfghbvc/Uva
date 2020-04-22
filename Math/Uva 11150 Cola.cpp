7cfe8c091fbf0bf92e0cbe7dd36034879969949e
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-04-23 00:25:44 +0800

    Trick

---
+++ Math/Uva 11150 Cola.cpp
@@ -0,0 +1,15 @@
+//Trick math solution.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+    while(cin >> n){
+    	cout << n+n/2 << endl;
+    }
+    return 0;
+}
+/**
+110 110 110 1
+ 1   1   1  0 
+**/
