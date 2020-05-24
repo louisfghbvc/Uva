9ec16be436efb84d9e9e56305eef6b00a0daf147
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-05-24 16:33:26 +0800

    Case check

--- Math/Uva 10242 Fourth Point!!.cpp
+++ Math/Uva 10242 Fourth Point!!.cpp
@@ -1,4 +1,9 @@
-//make b, c same.
+// make b, c same.
+// a c same. swap
+// a d same.
+// b d same.
+// c d same.
+
 #include <bits/stdc++.h>
 using namespace std;
 struct point{
@@ -9,17 +14,12 @@ int main(){
    	while(cin >> p[0].x >> p[0].y){
    		for(int i = 1; i < 4; ++i)
    			cin >> p[i].x >> p[i].y;
-   		if(p[0].x == p[2].x && p[0].y == p[2].y)
-   			swap(p[0], p[1]);
-   		else if(p[3].x == p[1].x && p[3].y == p[1].y)
-   			swap(p[2], p[3]);
-   		if(p[0].x == p[3].x && p[0].y == p[3].y){
-   			swap(p[0], p[1]); swap(p[2], p[3]);
-   		}
-   		if(p[0].x == p[3].x && p[0].y == p[3].y){
-   			swap(p[0], p[1]); swap(p[2], p[3]);
-   		}
    			
+   		if(p[0].x == p[2].x && p[0].y == p[2].y) swap(p[0], p[1]);
+   		if(p[0].x == p[3].x && p[0].y == p[3].y) swap(p[0], p[1]), swap(p[2], p[3]);
+   		if(p[1].x == p[3].x && p[1].y == p[3].y) swap(p[2], p[3]);
+   		if(p[2].x == p[3].x && p[2].y == p[3].y) swap(p[1], p[3]);
+		
    		printf("%.3f %.3f\n", p[0].x+p[3].x-p[2].x, p[0].y+p[3].y-p[2].y);
    	}
     return 0;
