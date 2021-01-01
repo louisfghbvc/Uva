c164d59b887289b9ab38d35af597d6f7ddbbb5f0
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-01-01 11:29:10 +0800

    Window, Two Pointer.

--- Sliding Window/Uva 11536 Smallest Sub-Array.cpp
+++ Sliding Window/Uva 11536 Smallest Sub-Array.cpp
@@ -1,4 +1,4 @@
-// Maintain window number size = k.
+// Maintain window number size = k. use map or array.
 // O(N).
 
 #include <bits/stdc++.h>
@@ -13,18 +13,20 @@ void solve(){
     for(int i = 3; i < n; ++i)
         arr[i] = (arr[i-1] + arr[i-2] + arr[i-3]) % m + 1;
 
-    unordered_map<int, int> mp;
+    int mp[105] = {}, mp_size = 0;
     int res = -1;
     for(int r = 0, l = 0; r < n; ++r){
-        if(mp.size() < k && arr[r] <= k) mp[arr[r]]++;
-        if(mp.size() == k){
+        if(mp_size < k && arr[r] <= k) {
+            if(mp[arr[r]]++ == 0) mp_size++;
+        }
+        if(mp_size == k){
             if(res == -1) res = r-l+1;
             else res = min(res, r-l+1);
-            while(mp.size() >= k){
+            while(mp_size >= k){
                 res = min(res, r-l+1);
                 int tmp = arr[l++];
                 if(tmp > k) continue;
-                if(--mp[tmp] == 0) mp.erase(tmp);
+                if(--mp[tmp] == 0) mp_size--;
             }
         }
     }
@@ -44,3 +46,4 @@ int main() {
         solve();
     }
 }
+
